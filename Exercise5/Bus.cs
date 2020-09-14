7b5933099563f7200838bcafc818eba6fded5eef
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-09-14 16:46:51 +0200

    Add Bus

---
+++ Exercise5/Bus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    public class Bus : Vehicle
+    {
+        public int Seats { get; set; }
+
+
+        public Bus(string regNumber, string color, int numberOfWheels, int seats)
+            : base(regNumber, color, numberOfWheels)
+        {
+            Seats = seats;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} Seats: {Seats}";
+        }
+    }
+}
