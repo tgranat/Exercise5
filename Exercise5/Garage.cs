4a1e5e77bfb9d366c2d565baa47a713ed997b18a
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-09-14 13:32:44 +0200

    Start implementing Garage<T>

---
+++ Exercise5/Garage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    public class Garage<T> : IEnumerable<T> where T : Vehicle
+    {
+        private Vehicle[] garage;
+
+        public Garage(int sizeOfGarage)
+        {
+            garage = new Vehicle[sizeOfGarage];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
