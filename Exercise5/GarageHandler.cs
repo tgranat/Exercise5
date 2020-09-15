1ff6955e420c5c03be69e25c95764584393aa9a9
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-09-15 09:46:09 +0200

    Handler for Garage

---
+++ Exercise5/GarageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    // För abstraktion
+    // TODO extract interface
+    public class GarageHandler
+    {
+        // create Garage (factory)
+
+        // implement functionality for UI:
+        // AddVehicle(IGarage garage, IVehicle vehicle)  ...
+
+        // IVehicle car = Handler.CreateCar(.....);  ??
+    }
+}
