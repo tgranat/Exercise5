196da68ac9381590bc3f19706ca156da40f8b955
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-09-15 13:11:02 +0200

    Rename

---
+++ Exercise5.Tests/VehicleTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercise5.Tests
+{
+    [TestClass]
+    public class VehicleTest
+    {
+        [TestMethod]
+        public void Car_CreateObject_Success()
+        {
+            Car car = new Car("ABC123", "Green", 4, FuelType.Diesel);
+            Assert.AreEqual(FuelType.Diesel, car.Type);
+        }
+
+        [TestMethod]
+        public void Bus_CreateObject_Success()
+        {
+            Bus bus = new Bus("ABC123", "Green", 4, 40);
+            Assert.AreEqual(40, bus.Seats);
+        }
+    }
+}
