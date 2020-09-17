37f2cad27ebf205119fa55e2a2a75d7b91b57316
Torbjorn Granat <torbjorn.granat@gmail.com>
2020-09-17 11:14:21 +0200

    Update test

--- Exercise5.Tests/GarageHandlerTest.cs
+++ Exercise5.Tests/GarageHandlerTest.cs
@@ -8,29 +8,11 @@ namespace Exercise5.Tests
     class GarageHandlerTest
     {
         [TestMethod]
-        public void GarageHandler_CreateCar_Success()
+        public void GarageHandler_Garage_Success()
         {
             GarageHandler h = new GarageHandler();
             Garage<IVehicle> g = h.CreateGarage(10);
-            bool result = h.CreateCar(g, "ABC123", "Green", 4, FuelType.Diesel);
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void GarageHandler_CreateBus_Success()
-        {
-            GarageHandler h = new GarageHandler();
-            Garage<IVehicle> g = h.CreateGarage(10);
-            Assert.IsTrue(h.CreateBus(g, "ABC123", "Green", 4, 40));
-        }
-        [TestMethod]
-        public void GarageHandler_RemoveVehicle_Success()
-        {
-            GarageHandler h = new GarageHandler();
-            Garage<IVehicle> g = h.CreateGarage(10);
-            h.CreateBus(g, "ABC123", "Green", 4, 40);
-            h.CreateCar(g, "CDE123", "Green", 4, FuelType.Diesel);
-            Assert.IsTrue(h.RemoveVehicle(g, "ABC123"));          
+            Assert.IsNotNull(g);
         }
     }
 }
